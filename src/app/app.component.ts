1d77ab230b7a2bf18bf523a53fb0e89f5c60187b
sumi <sumnima9@gmail.com>
2017-08-18 21:59:36 +0100

    UI half complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
 
+export class O {
+  value: string;
+
+  constructor() {
+    this.value = null;
+  }
+}
+
+export class Board {
+  name: string;
+  board: any[10][10];
+ 
+  constructor(name: string) {
+    this.name = name;
+    this.board = [
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()],
+      [new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O(), new O()]
+    ];
+    }
+
+    place_all_ships() {
+        
+    }
+
+    place_battle_ship() {
+    }
+}
+
+export class Player {
+  name: string;
+  player_map: Board;
+  opponent_map: Board;
+}
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'My First Angular App';
+  title = 'Battleships';
+  player: Player = {
+    name: 'You',
+    player_map: new Board('You'),
+    opponent_map: new Board('Opponent')
+  };
+  onSelect(cell: O): void {
+    cell.value = "X";
+  }
 }
